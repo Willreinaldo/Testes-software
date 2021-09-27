9b1f06300c79b9964d1343e2566096183f755a54
Willian Reinaldo <48501869+Willreinaldo@users.noreply.github.com>
2021-09-27 09:07:59 -0300

    Update main_teste_sistema.spec.js

--- testes de sistema e integracao/cypress/integration/main_teste_sistema.spec.js
+++ testes de sistema e integracao/cypress/integration/main_teste_sistema.spec.js
@@ -3,16 +3,18 @@
       cy.visit("http://localhost:8080");
     });
 
-    it("Deve ser possivel responder uma pergunta", () => {
-      cy.get("#responder-button").click()
-      cy.get("#resposta-body").type("teste resposta");
-      cy.get("#resposta-button").click()
-    });
-
     it("Deve fazer uma pergunta", () => {
       cy.get("#perguntar").click()
       cy.get("#perguntar-titulo").type("teste pergunta");
       cy.get("#perguntar-descricao").type("teste pergunta descricao");
       cy.get("#button-perguntar").click();
     });
+
+    it("Deve ser possivel responder uma pergunta", () => {
+      cy.visit("http://localhost:8080");
+      cy.get("#responder-button").click()
+      cy.get("#resposta-body").type("teste resposta");
+      cy.get("#resposta-button").click()
+    });
+
 });
